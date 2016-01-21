20787b14c897216a7bd75cc337b9782c7ef62302
zepumph <mjkauzmann@gmail.com>
2016-01-21 13:49:41 -0700

    Added logic to play every note

--- app/src/main/java/mobilecomputing/com/audioplayground/MainActivity.java
+++ app/src/main/java/mobilecomputing/com/audioplayground/MainActivity.java
@@ -1,48 +1,82 @@
 package mobilecomputing.com.audioplayground;
 
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.Resources;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
-import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
 
-    final String notes[] = {"A", "A#/Bb", "B", "C", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab"};
-    String currentNote = "";
+
+public class MainActivity extends Activity {
+
+    public static final String ERROR = "ERROR";
+
+
+    final String notes[] = {"A", "Bb", "B", "C", "Eb", "E", "F", "Gb", "G", "Ab"};
+    String currentNote = "note_a";
+
+    MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.note_a);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView txtPickerOutput = (TextView) findViewById(R.id.txtOutput);
-
-
-        NumberPicker notePicker = (NumberPicker) findViewById(R.id.notePicker);
+        NumberPicker notePicker = (NumberPicker) findViewById(R.id.note_picker);
         notePicker.setMinValue(0);
         notePicker.setMaxValue(notes.length - 1);
         notePicker.setDisplayedValues(notes);
         NumberPicker.OnValueChangeListener notePickerListener = new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                txtPickerOutput.setText("Value: " + notes[newVal]);
+                currentNote = "note_" + notes[newVal].toLowerCase();
             }
-
         };
-
-
         notePicker.setOnValueChangedListener(notePickerListener);
 
-        Button playButton = (Button) findViewById(R.id.btn_play);
-        playButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final MediaPlayer myMediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.note_e);
-                myMediaPlayer.start();
-            }
-        });
+    }
+
+
+    public void playStop(View view){
+        final Button playButton = (Button) findViewById(R.id.btn_play);
+        int note_id = this.getResources().getIdentifier(currentNote, "raw",
+                this.getPackageName());
+        try {
+//            String path = getExternalFilesDir(null).toString() + "/";
+            Log.d("MEDIA_PLAYER", "IN PATH:  " + path);
+
+//            mp.setDataSource(;
+        }
+        catch (Resources.NotFoundException e){
+            mp = MediaPlayer.create(MainActivity.this, R.raw.note_e);
+
+            Log.e(ERROR, "Defaulting to note E:  " + e.getMessage(),e);
+
+        }
+//        Toast t = Toast.makeText(getApplicationContext(), currentNote, Toast.LENGTH_LONG);
+//        t.show();
+
+        if( playButton.getText().equals(getString(R.string.play))) {
+            playButton.setText(R.string.stop);
+            mp.start();
+        }
+        else if (playButton.getText().equals((getString(R.string.stop)))){
+            playButton.setText(R.string.play);
+            Log.d("MEDIA_PLAYER", "IN STOP:    ");
+
+            mp.pause();
+            mp.reset();
+//            myMediaPlayer.reset();
+        }
+        else{
+            Log.e(ERROR, "playbutton not set correctly");
+        }
     }
 }
