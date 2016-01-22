e8bb7779f82f6df4d1d4c601651cf478918e8642
Berto Lopez-Cruz <bertolopez-cruz@Robertos-MacBook-Pro.local>
2016-01-22 00:38:06 -0700

    splash and background

---
+++ app/src/main/java/mobilecomputing/com/audioplayground/LauncherActivity.java
@@ -0,0 +1,31 @@
+package mobilecomputing.com.audioplayground;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LauncherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen_layout);
+
+        Intent transition = new Intent(LauncherActivity.this, MainActivity.class);
+
+
+            LauncherActivity.this.startActivity(transition);
+
+        new Handler().postDelayed(new Runnable(){
+                @Override
+                public void run() {
+                    Intent transition = new Intent(LauncherActivity.this, MainActivity.class);
+                    LauncherActivity.this.startActivity(transition);
+                    LauncherActivity.this.finish();
+                }
+            }, 5000);
+        }
+
+
+    }
+
